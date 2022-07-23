8830de9f38fc56feecdc5b98a370c2278e2ee31d
buenorafa <rafaelbuenooo@outlook.com>
2022-07-23 09:29:02 -0300

    Add preventDefault();

--- ping-coming-soon-page-master/script.js
+++ ping-coming-soon-page-master/script.js
@@ -1,17 +1,23 @@
+
 const emailInput = document.getElementById("email-input");
 const errorMsg = document.getElementById("error-msg");
 const submitBtn = document.getElementById("submit-btn");
 
 const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-function validation(){
+function validation(e){
+    e.preventDefault();
     let email = emailInput.value;
     if(email === ""){
         errorMsg.innerText = "Whoops! It looks like you forgot to add your email";
         errorMsg.style.display = "block";
         emailInput.style.borderColor = "var(--light-red)";
     }
-    else if(!email.match(validRegex)){
+    else if(email.match(validRegex)){
+        errorMsg.innerText = "";
+        emailInput.style.border = "2px solid green";
+    }
+    else if(!email.match(validRegex)){ 
         errorMsg.innerText = "Please provide a valid email address";
         errorMsg.style.display = "block";
         emailInput.style.borderColor = "var(--light-red)";
